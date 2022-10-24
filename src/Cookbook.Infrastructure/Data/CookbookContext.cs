908700744e56519c382a541aebba0e1d2f08b02f
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2022-10-24 15:03:56 -0300

    set up initial api configuration and data context

---
+++ src/Cookbook.Infrastructure/Data/CookbookContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cookbook.Infrastructure.Data;
+
+public class CookbookContext : DbContext
+{
+    public CookbookContext(DbContextOptions<CookbookContext> options) : base(options)
+    {
+
+    }
+}
