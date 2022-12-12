4290a519b566cd1c7bd7c08b59d3dc239a5f74b3
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2022-12-12 10:10:17 -0300

    Rename function name

--- src/Cookbook.Domain/Interfaces/Repository/IUserRepository.cs
+++ src/Cookbook.Domain/Interfaces/Repository/IUserRepository.cs
@@ -5,5 +5,5 @@ namespace Cookbook.Domain.Interfaces.Repository;
 public interface IUserRepository : IRepositoryBase<User>
 {
     Task<bool> ExistsAsync(string email);
-    Task<User> LoginAsync(string email, string password);
+    Task<User> GetByEmailAndPasswordAsync(string email, string password);
 }
--- src/Cookbook.Infrastructure/Data/Repository/UserRepository.cs
+++ src/Cookbook.Infrastructure/Data/Repository/UserRepository.cs
@@ -12,6 +12,6 @@ public class UserRepository : RepositoryBase<User>, IUserRepository
 
     public async Task<bool> ExistsAsync(string email) => await DbSet.AnyAsync(x => x.Email == email);
 
-    public async Task<User> LoginAsync(string email, string password) 
+    public async Task<User> GetByEmailAndPasswordAsync(string email, string password) 
 		=> await DbSet.AsNoTracking().FirstOrDefaultAsync(c => c.Email.Equals(email) && c.PasswordHash.Equals(password));
 }
