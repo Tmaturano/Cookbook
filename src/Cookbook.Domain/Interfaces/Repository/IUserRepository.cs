86083b60bbb5ae59d95807a1c09ff09cea625559
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2022-12-12 10:09:09 -0300

    Add login function to the repository

--- src/Cookbook.Domain/Interfaces/Repository/IUserRepository.cs
+++ src/Cookbook.Domain/Interfaces/Repository/IUserRepository.cs
@@ -5,4 +5,5 @@ namespace Cookbook.Domain.Interfaces.Repository;
 public interface IUserRepository : IRepositoryBase<User>
 {
     Task<bool> ExistsAsync(string email);
+    Task<User> LoginAsync(string email, string password);
 }
--- src/Cookbook.Infrastructure/Data/Repository/UserRepository.cs
+++ src/Cookbook.Infrastructure/Data/Repository/UserRepository.cs
@@ -11,4 +11,7 @@ public class UserRepository : RepositoryBase<User>, IUserRepository
 	}
 
     public async Task<bool> ExistsAsync(string email) => await DbSet.AnyAsync(x => x.Email == email);
+
+    public async Task<User> LoginAsync(string email, string password) 
+		=> await DbSet.AsNoTracking().FirstOrDefaultAsync(c => c.Email.Equals(email) && c.PasswordHash.Equals(password));
 }
