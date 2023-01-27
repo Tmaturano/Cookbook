25112a221505036bfab44361cdd3770c13788427
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-01-27 09:36:12 -0300

    feat: JWT + Swagger refinements

--- src/Cookbook.API/Program.cs
+++ src/Cookbook.API/Program.cs
@@ -1,6 +1,7 @@
 using Cookbook.API.Extensions;
 using Cookbook.API.Filters;
 using Cookbook.API.Middleware;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,32 @@ builder.ConfigureServices();
 builder.LoadConfiguration();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Cookbook API", Version= "1.0" });
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "JWT Authorization header with Bearer scheme. Example: \"Authorization: Bearer {token}\""
+    }) ;
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddScoped<AuthenticatedUserAttribute>();
 
